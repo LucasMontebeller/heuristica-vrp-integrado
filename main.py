ddb20aff8cd76f23f19f1aab9e2b3e0096567c58
Lucas Felipe Bravim Montebeller <lmontebeller@mogai.com.br>
2025-02-08 21:12:50 -0300

    Testando outros experimentos

--- main.py
+++ main.py
@@ -21,37 +21,37 @@ class Dados:
         - T_volta: Lista com o tempo de volta do lote à fábrica (calculado como 1.15 vezes o tempo de ida).
         - DE: Matriz (a, b) que representa o tempo de deslocamento entre os talhões 'a' e 'b'.
         """
-        self.nL = 6
+        self.nL = 8
         self.nT = 3
-        self.nV = 2
+        self.nV = 3
         self.nE = 2
-        self.TC = 0.5
+        self.TC = 1
 
         self.V = [v for v in range(1, self.nV + 1)]
         self.E = [e for e in range(1, self.nE + 1)]
         self.L = [l for l in range(1, self.nL + 1)]
         self.T = [a for a in range(1, self.nT + 1)]
 
-        self.LT = [2, 3, 1]
+        self.LT = [3, 3, 2]
         
         # total de linhas = total de talhões 'a' + 2 virtuais
         # total de colunas = total de lotes 'i'
         self.LE = [ 
-            [0,	0,	0,	0,	0,	0],
-            [1,	1,	0,	0,	0,	0],
-            [0,	0,	1,	1,	1,	0],
-            [0,	0,	0,	0,	0,	1],
-            [0,	0,	0,	0,	0,	0],
+                [0,	0,	0,	0,	0,	0,	0,	0],
+                [1,	1,	1,	0,	0,	0,	0,	0],
+                [0,	0,	0,	1,	1,	1,	0,	0],
+                [0,	0,	0,	0,	0,	0,	1,	1],
+                [0,	0,	0,	0,	0,	0,	0,	0]
         ]
         
-        self.T_ida = [2, 2, 1.5, 1.5, 1.5, 3]
+        self.T_ida = [0.78, 0.78, 0.78, 1.57, 1.57, 1.57, 0.2, 0.2]
         self.T_volta = [1.15 * t for t in self.T_ida]
         
         self.DE = [
             [0,  0,  0,  0, 0],
-            [0,  0, 10, 12, 0],
-            [0, 10,  0,  9, 0],
-            [0, 12,  9,  0, 0],
+            [0,  0, 3.03, 1.24, 0],
+            [0, 3.03,  0,  2.15, 0],
+            [0, 1.24,  2.15,  0, 0],
             [0,  0,  0,  0, 0],
         ]
         
@@ -366,3 +366,8 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+## Rever experimentos:
+# Exp08_02
+# Exp08_04
+# Exp08_05
