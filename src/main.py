70b03637ce77221a7d10cd8479a114d79d04d686
Lucas Felipe Bravim Montebeller <lmontebeller@mogai.com.br>
2025-02-22 13:14:11 -0300

    Melhorando entrada de dados

--- src/main.py
+++ src/main.py
@@ -1,10 +1,37 @@
+import json
+import os
 from solver import Dados, Modelo, Heuristica
 
+def carregar_dados() -> list[Dados]:
+    pasta = "data"
+    arquivos_json = [f for f in os.listdir(pasta) if f.endswith(".json")]
+    
+    dados_lista = []
+    for arquivo in arquivos_json:
+        caminho_completo = os.path.join(pasta, arquivo)
+        try:
+            with open(caminho_completo, "r", encoding="utf-8") as f:
+                dados_json = json.load(f)
+                dados_lista.append((arquivo, Dados(**dados_json)))
+        except (json.JSONDecodeError, OSError, TypeError) as e:
+            print(f"Erro ao carregar {arquivo}: {e}")
+    
+    return dados_lista
+
+def executa_instancias(instancias: list[tuple[str, Dados]]) -> dict[str, any]:
+    solucoes = {}
+    for arquivo, dados in instancias:
+        modelo = Modelo(dados)
+        heuristica = Heuristica(modelo)
+        solucao, iteracoes = heuristica.simulated_annealing(max_exec=20000)
+        solucoes[arquivo] = solucao
+    return solucoes
+
 def main():
-    dados = Dados()
-    modelo = Modelo(dados)
-    heuristica = Heuristica(modelo)
-    solucao, iteracoes = heuristica.simulated_annealing(max_exec=20000)
+    dados = carregar_dados()
+    solucoes = executa_instancias(dados)
+    for arquivo, sol in solucoes.items():
+        print(f"{arquivo} - {sol.M}")
 
 if __name__ == "__main__":
     main()
--- src/solver/dados.py
+++ src/solver/dados.py
@@ -1,6 +1,6 @@
 class Dados:
     """Classe que armazena os dados do problema."""
-    def __init__(self):
+    def __init__(self, nV, nE, TC, T_ida, LE, DE):
         """Atributos:
         ----------
         - nL: Total de lotes disponíveis.
@@ -18,36 +18,19 @@ class Dados:
         - T_volta: Lista com o tempo de volta do lote à fábrica (calculado como 1.15 vezes o tempo de ida).
         - DE: Matriz (a, b) que representa o tempo de deslocamento entre os talhões 'a' e 'b'.
         """
-        self.nL = 8
-        self.nT = 3
-        self.nV = 3
-        self.nE = 2
-        self.TC = 1
+        self.nV = nV
+        self.nE = nE
+        self.TC = TC
+        self.T_ida = T_ida
+        self.LE = LE # total de linhas = total de talhões 'a' + 2 virtuais | total de colunas = total de lotes 'i'
+        self.DE = DE
+
+        self.T_volta = [1.15 * t for t in self.T_ida]
+        self.nT = len(self.LE) - 2 # removendo os virtuais
+        self.nL = len(self.LE[1])
+        self.LT = [sum(row) for row in self.LE[1:-1]] # desconsidera o primeiro e ultimo, que são virtuais
 
         self.V = [v for v in range(1, self.nV + 1)]
         self.E = [e for e in range(1, self.nE + 1)]
         self.L = [l for l in range(1, self.nL + 1)]
         self.T = [a for a in range(1, self.nT + 1)]
-
-        self.LT = [3, 3, 2]
-        
-        # total de linhas = total de talhões 'a' + 2 virtuais
-        # total de colunas = total de lotes 'i'
-        self.LE = [ 
-                [0,	0,	0,	0,	0,	0,	0,	0],
-                [1,	1,	1,	0,	0,	0,	0,	0],
-                [0,	0,	0,	1,	1,	1,	0,	0],
-                [0,	0,	0,	0,	0,	0,	1,	1],
-                [0,	0,	0,	0,	0,	0,	0,	0]
-        ]
-        
-        self.T_ida = [0.78, 0.78, 0.78, 1.57, 1.57, 1.57, 0.2, 0.2]
-        self.T_volta = [1.15 * t for t in self.T_ida]
-        
-        self.DE = [
-            [0,  0,  0,  0, 0],
-            [0,  0, 3.03, 1.24, 0],
-            [0, 3.03,  0,  2.15, 0],
-            [0, 1.24,  2.15,  0, 0],
-            [0,  0,  0,  0, 0],
-        ]
