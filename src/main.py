515df9e28435d8fcd7c8cdbd971813a576ebe85c
Lucas Felipe Bravim Montebeller <C0673459@vale.com>
2025-05-02 16:33:27 -0300

    Primeira versao do gerador de solucao vizinha

--- src/main.py
+++ src/main.py
@@ -30,15 +30,15 @@ def executa_instancias(instancias: list[tuple[str, Dados]]) -> dict[str, dict]:
         modelo = Modelo(dados)
         heuristica = Heuristica(modelo)
         # TODO: Executar random_search várias vezes e plotar gráfico de convergência x iteracoes. Isso vai dar o quão restrintivo o gerador de solucoes está.
-        solucao, iteracoes, iteracoes_convergencia = heuristica.random_search(max_exec=1000)
-        # solucao, iteracoes = heuristica.simulated_annealing()
+        # solucao, iteracoes, iteracoes_convergencia = heuristica.random_search(max_exec=1000)
+        solucao, iteracoes = heuristica.simulated_annealing()
         
         tempo_execucao = time.time() - inicio
         
         solucoes[arquivo] = {
             "solucao": solucao,
             "tempo_execucao": tempo_execucao,
-            "iteracoes_convergencia": iteracoes_convergencia
+            "iteracoes_convergencia": 0 #iteracoes_convergencia
         }
 
     return solucoes
